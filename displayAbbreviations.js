258244e3a14ef44f92c20ca95aed0bc7feeae71a
ih5-tande <511508730@qq.com>
2018-12-18 18:34:21 +0800

    enen

--- displayAbbreviations.js
+++ displayAbbreviations.js
@@ -1,14 +1,18 @@
 function displayAbbreviations() {
+    if(!document.getElementsByTagName||!document.createTextNode||!document.createElement) return false;
+   //取得所有缩略语
     var abbreviations=document.getElementsByTagName("abbr");
     if(abbreviations.length<1) return false;
     var dlist=document.createElement("dl");
     var defs=new Array();
+    //给数组添加所有abbr
     for(var i=0;i<abbreviations.length;i++) {
         var current_abbr = abbreviations[i];
         var key = current_abbr.firstChild.nodeValue;
         var definition = current_abbr.getAttribute("title");
         defs[key] = definition;
     }
+    //利用数组defs，给dlist 添加title和description节点
     for(key in defs) {
        var definition=defs[key];
 
@@ -21,7 +25,7 @@ function displayAbbreviations() {
         dlist.appendChild(dtitle);
         dlist.appendChild(ddes);
     }
-
+    //追加到<h2>标签里，将h2追加到body
     var header=document.createElement("h2");
     var header_text=document.createTextNode("Abbreviations");
     header.appendChild(header_text);
